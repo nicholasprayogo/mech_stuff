1045806a8832375887cb0d018ac29369c08af117
nicholasprayogo <nicholasandrien@live.com>
2019-11-27 20:56:15 -0800

    aggregate graph

--- photoelastic_stress_lab/graph.py
+++ photoelastic_stress_lab/graph.py
@@ -6,17 +6,22 @@ load10 = pd.read_csv("data/load10.csv")
 load12 = pd.read_csv("data/load12.csv")
 load14 = pd.read_csv("data/load14.csv")
 
+fig, ax = plt.subplots(2, 2)
+
 for index, load_data in enumerate([load8,load10,load12,load14]):
+    ax_dict = {0:ax[0,0],1:ax[0,1],2:ax[1,0],3:ax[1,1]}
+
     x_null = load_data["null_order"]  # fringe location
     y_null = load_data["null_strain"]
     x_tardy = load_data["tardy_order"]
     y_tardy = load_data["tardy_strain"]
 
-    plt.plot(x_null,y_null, label = 'Null', marker='1')
-    plt.plot(x_tardy,y_tardy, label = 'Tardy', marker = '.')
-
-    plt.xlabel("Corrected Fringe Order")
-    plt.ylabel("Strain Difference")
-    plt.legend()
-    plt.savefig(fname="load{}.png".format(8+index*2))
-    plt.show()
+    ax_dict[index].plot(x_null,y_null, label = 'Null', marker='1')
+    ax_dict[index].plot(x_tardy,y_tardy, label = 'Tardy', marker = '.')
+    ax_dict[index].set_title("Load: {} lb".format(8+index*2))
+    ax_dict[index].set_xlabel("Corrected Fringe Order")
+    ax_dict[index].set_ylabel("Strain Difference")
+    ax_dict[index].legend()
+plt.tight_layout()
+plt.savefig(fname="all_plots.png")
+plt.show()
